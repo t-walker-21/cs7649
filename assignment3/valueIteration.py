a1abce03d11daa54248fd462dd4b63984930850c
tevon walker <tjw0018@uah.edu>
2019-03-05 12:16:04 -0500

    finished VI implementation

--- assignment3/valueIteration.py
+++ assignment3/valueIteration.py
@@ -7,10 +7,31 @@ init_dist = [1,0,0,0]
 actions = [0,1]
 trans_mat1 = [[0.1,0.9,0,0],[0.9,0.1,0,0],[0,0,0.1,0.9],[0,0,0,1]]
 trans_mat2 = [[0.9,0.1,0,0],[0,0.1,0,0.9],[0.9,0,0.1,0],[0,0,0,1]]
-reward = [0,0,0,1]
+reward = [0,1,0,10]
 discount_factor = 0.95
 
 #initialize value function
 
 value = [0,0,0,0]
+
+horiz = 1000
+
+
+for h in range(0,horiz): #finite horizion of 100
+    for s in range(0,len(init_dist)):
+        tempMax = -1E11 # technically, in an mdp, there can exists negative reward, so initialize temp at large neg value
+        value_prev = value[:]
+        for a in actions: #must find max value w.r.t action
+            total = 0
+            for q in range(0,len(init_dist)): #must sum over future states
+                if (a == 0): #must apply correct transition matrix
+                    total += trans_mat1[s][q]*(reward[s] + discount_factor*value_prev[q])
+                else:
+                    total += trans_mat2[s][q]*(reward[s] + discount_factor*value_prev[q])
+            if total > tempMax:
+                tempMax = total
+        
+        value[s] = tempMax
+
+    print value
 
