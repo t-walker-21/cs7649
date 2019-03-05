b6bc4ec147faeee15d31b83b5c809a9f7f722ae8
tevon walker <tjw0018@uah.edu>
2019-03-05 17:19:09 -0500

    added loop to forward and plotting VI

--- assignment3/forwardAlgo.py
+++ assignment3/forwardAlgo.py
@@ -2,7 +2,7 @@
 
 #HMM
 trans_matrix = [[0.1,0.4,0.5],[0.4,0,0.6],[0,0.6,0.4]]
-sensor_matrix = [[0.2,0.2,0.2],[0.6,0.6,0.2],[0.2,0.2,0.6]]
+sensor_matrix = [[0.6,0.2,0.2],[0.2,0.6,0.2],[0.2,0.2,0.6]]
 initial_dist = [1,0,0]
 obser_seq = [1,3,3]
 obser_dict = {1:0,2:1,3:2}
@@ -13,7 +13,23 @@ obser_dict = {1:0,2:1,3:2}
 
 alpha = [[],[],[]]
 
+#initialize alpha
 for s in range(0,len(initial_dist)):
     alpha[s] = initial_dist[s]*sensor_matrix[s][obser_dict[obser_seq[0]]]
 
 print alpha
+
+
+#compute alphas
+for z in range(0,len(obser_seq)-1): #for each observation in the sequence...
+    alpha_prev = alpha[:] #make a copy of alpha vector from previous iteration
+    #print alpha_prev
+    
+    for s in range(0,len(initial_dist)): #loop over states s
+        total = 0 #sum for sigma term
+        for q in range(0,len(initial_dist)): #loop over states we can possibly transition to
+            total += alpha_prev[q]*trans_matrix[q][s]
+        
+        alpha[s] = sensor_matrix[s][obser_dict[obser_seq[z+1]]] * total
+
+    print alpha 
