ba2643d4bbc4b9f590f5f9b78a6da738175bcb3c
tevon walker <tjw0018@uah.edu>
2019-03-05 14:24:37 -0500

    plotted values of each state

--- assignment3/valueIteration.py
+++ assignment3/valueIteration.py
@@ -1,25 +1,31 @@
 #Tevon Walker CS7649 -- Homework 3B problem 7
 
 #mdp definition
+import matplotlib.pyplot as plt
 
 states = [[],[],[],[]]
 init_dist = [1,0,0,0]
 actions = [0,1]
 trans_mat1 = [[0.1,0.9,0,0],[0.9,0.1,0,0],[0,0,0.1,0.9],[0,0,0,1]]
 trans_mat2 = [[0.9,0.1,0,0],[0,0.1,0,0.9],[0.9,0,0.1,0],[0,0,0,1]]
-reward = [0,1,0,10]
-discount_factor = 0.95
+reward = [0,0,0,1]
+discount_factor = 0.5
 
 #initialize value function
 
 value = [0,0,0,0]
 
-horiz = 1000
+horiz = 20
 
+vals1 = []
+vals2 = []
+vals3 = []
+vals4 = []
 
 for h in range(0,horiz): #finite horizion of 100
+    
     for s in range(0,len(init_dist)):
-        tempMax = -1E11 # technically, in an mdp, there can exists negative reward, so initialize temp at large neg value
+        tempMax = float('-inf') # technically, in an mdp, there can exists negative reward, so initialize temp at large neg value
         value_prev = value[:]
         for a in actions: #must find max value w.r.t action
             total = 0
@@ -32,6 +38,28 @@ for h in range(0,horiz): #finite horizion of 100
                 tempMax = total
         
         value[s] = tempMax
+        if (s == 0):
+            vals1.append(tempMax)
+        
+        if (s == 1):
+            vals2.append(tempMax)
+
+        if (s == 2):
+            vals3.append(tempMax)
+
+        if (s == 3):
+            vals4.append(tempMax)
 
     print value
 
+plt.plot(vals1,label='s1')
+plt.plot(vals2,label='s2')
+plt.plot(vals3,label='s3')
+plt.plot(vals4,label='s4')
+plt.title('Value of each state as a function of iterations of Value Iterations. Eps = 0.5')
+plt.xlabel("Iterations")
+plt.ylabel("Value of State")
+
+plt.legend(loc='upper left')
+
+plt.show()
